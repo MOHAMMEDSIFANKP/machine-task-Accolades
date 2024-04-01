197e22418a550643dfb9432fea13308efcb89a57
sifan <sifan007sifu@gmail.com>
2024-04-01 12:51:34 +0530

    Footer responceive completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -14,21 +14,21 @@ import { MdCopyright } from "react-icons/md";
 
 function Footer() {
   return (
-    <div className='h-screen grid grid-rows-[10rem,1px,1fr]'>
+    <div className='lg:h-screen grid grid-rows-[10rem,1px,1fr]'>
       <div className='flex justify-center items-center'>
         <img src={Logo} className='h-16 w-64 ' alt="" />
       </div>
       <hr className='mx-auto container border border-gray-900' />
-      <div className='mx-auto container grid grid-rows-[1fr,6rem,12rem]  bg-no-repeat bg-contain bg-center' style={{ backgroundImage: `url(${BgImage})` }}>
-        <div className='grid grid-cols-[1fr,5rem,1fr] gap-8'>
-          <Card className='bg-gray-900 bg-opacity-50 border grid grid-cols-[5rem,1fr] border-gray-800 my-10 rounded-[2rem]'>
-            <div className='text-red-500 ps-7'>
+      <div className='mx-auto container grid lg:grid-rows-[1fr,6rem,12rem]  bg-no-repeat bg-contain bg-center' style={{ backgroundImage: `url(${BgImage})` }}>
+        <div className='lg:grid lg:grid-cols-[1fr,5rem,1fr] gap-8'>
+          <Card className='bg-gray-900 bg-opacity-50 border grid grid-cols-[3rem,1fr] lg:grid-cols-[5rem,1fr] border-gray-800 my-10 rounded-[2rem]'>
+            <div className='text-red-500 ps-3 lg:ps-7'>
               <IoLocationSharp className='h-7 w-7 mt-[7rem]' />
-              <MdEmail className='h-6 w-6 mt-[5.3rem]' />
+              <MdEmail className='h-6 w-6 mt-[7.5rem] lg:mt-[5.3rem]' />
               <FaPhoneAlt className='h-6 w-6 mt-[2rem]' />
 
             </div>
-            <div className='text-xl text-white opacity-90 me-5 leading-8'>
+            <div className=' lg:text-xl text-white opacity-90 me-5 leading-8 '>
               <p className='pt-10'>NAVI MUMBAI </p>
               <p className='pt-8'>National Builders Office, Sea  Queen Heritage Buiding, 1st Floor, Plot No, Sector 18, Palm Beach Service Road, Sanpada, Navi Mumbai-400705 </p>
               <p className='pt-5'>marketingmumbai@nationalbuilders.in</p>
@@ -37,31 +37,30 @@ function Footer() {
 
           </Card>
 
-          <Card className='bg-gray-900 bg-opacity-50  border flex justify-center items-center border-gray-800 my-10 rounded-[2rem]'>
-            <FaFacebook className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full ' />
-            <GrInstagram className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full ' />
-            <FaLinkedin className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full ' />
-            <IoLogoYoutube className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full ' />
+          <div className='bg-gray-900 bg-opacity-50 border flex justify-center items-center border-gray-800 my-10 rounded-[2rem] sm:flex-col h-16 lg:h-auto'>
+            <FaFacebook className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full' />
+            <GrInstagram className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full' />
+            <FaLinkedin className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full' />
+            <IoLogoYoutube className='w-11 h-11 text-red-500 m-5 bg-black p-3 rounded-full' />
+          </div>
 
-
-          </Card>
-          <Card className='bg-gray-900 bg-opacity-50 border grid grid-cols-[5rem,1fr] border-gray-800 my-10 rounded-[2rem]'>
-            <div className='text-red-500 ps-7'>
+          <Card className='bg-gray-900 bg-opacity-50 border grid grid-cols-[3rem,1fr] lg:grid-cols-[5rem,1fr] border-gray-800 my-10 rounded-[2rem]'>
+          <div className='text-red-500 ps-3 lg:ps-7'>
               <IoLocationSharp className='h-7 w-7 mt-[7rem]' />
               <MdEmail className='h-6 w-6 mt-[4rem]' />
               <FaPhoneAlt className='h-6 w-6 mt-[2rem]' />
 
             </div>
-            <div className='text-xl text-white opacity-90 me-5 leading-8'>
+            <div className=' lg:text-xl text-white opacity-90 me-5 leading-8 '>
               <p className='pt-10'>KOCHI </p>
               <p className='pt-8'>National Pearl Star, 5Th Floor, High School Jn.Edappally, Kochi, Kerala - 682024 </p>
-              <p className='pt-9'>marketingkochi@nationalbuilders.in</p>
-              <p className='pt-5'>+91 484 2340781, 2333391, +91 484 2534558</p>
+              <p className='lg:pt-9'>marketingkochi@nationalbuilders.in</p>
+              <p className='lg:pt-10'>+91 484 2340781, 2333391, +91 484 2534558</p>
             </div>
 
           </Card>
         </div>
-        <Card className='rounded-[2rem] grid grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,9rem,1fr,1fr] pe-8 bg-opacity-50 text-white font-bold text-xl bg-gray-900 border border-gray-800'>
+        <Card className='rounded-[2rem] py-4 lg:py-0 lg:grid lg:grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,9rem,1fr,1fr] pe-8 bg-opacity-50 text-white font-bold lg:text-xl bg-gray-900 border border-gray-800'>
           <p className='flex justify-center items-center'>Home</p>
           <p className='flex justify-center items-center'>Projects</p>
           <p className='flex justify-center items-center'>Rentals</p>
@@ -72,8 +71,8 @@ function Footer() {
           <p className='flex justify-center items-center'>Blogs</p>
           <p className='flex justify-center items-center'>Key handover</p>
         </Card>
-        <Card className='rounded-[2rem] h-20 mt-10 grid grid-cols-3 text-white text-xl bg-gray-900 bg-opacity-50 border border-gray-900'>
-          <p className='flex justify-center items-center'><MdCopyright className='w-6 h-6' /> National Builders All Rights Reseved
+        <Card className='rounded-[2rem] lg:h-20 mt-10 lg:grid lg:grid-cols-3 py-3 text-white mb-10 lg:mb-0 lg:text-xl bg-gray-900 bg-opacity-50 border border-gray-900'>
+          <p className='flex justify-center items-center '><MdCopyright className='w-6 h-6' /> National Builders All Rights Reseved
           </p>
           <p className='flex justify-center items-center'>ACCOLADES Integrated</p>
           <p className='flex justify-center items-center'>Privacy | Terms of Service</p>
